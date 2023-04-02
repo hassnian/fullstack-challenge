1ff6827feb8b6db610bf1f60f17dbc0c9d500b5f
hassnian <hassnianxd@gmail.com>
2023-04-02 18:02:45 +0500

    added cron

--- server/app/Console/Kernel.php
+++ server/app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('app:feed-news-data-source')
+                 ->daily();
     }
 
     /**
