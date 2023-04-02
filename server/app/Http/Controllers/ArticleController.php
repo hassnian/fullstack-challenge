4d6010baba75b5ada207341619a067daacbe7b2c
hassnian <hassnianxd@gmail.com>
2023-04-02 19:36:30 +0500

    updated validation

--- server/app/Http/Controllers/ArticleController.php
+++ server/app/Http/Controllers/ArticleController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ArticleSearchQueryOptions;
+use App\Helpers\Enums\ArticleDatasourceType;
 use App\Http\Resources\ArticleResource;
 use App\Interfaces\ArticleSearchRepository;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ArticleController extends Controller
 {
@@ -21,11 +23,18 @@ class ArticleController extends Controller
         $page = request('page') ?? 1;
         $perPage = request('per_page') ?? 10;
 
-        //        $request->validate([
-//            'q' => 'required|string',
-//            'categories' => 'present|array|exists:categories,id',
-//            'datasources' => 'present|array|exists:datasources,id',
-//        ]);
+       $request->validate([
+            'q' => 'nullable|string',
+            'categories' => 'exists:categories,id',
+            'datasources' => ['nullable', Rule::in([
+                ArticleDatasourceType::NEW_YORK_TIMES,
+                ArticleDatasourceType::THE_GUARDIAN,
+                ArticleDatasourceType::NEW_YORK_TIMES,
+            ])],
+            'authors' => 'exists:authors,id',
+            'page' => 'nullable|integer',
+            'per_page' => 'nullable|integer'
+        ]);
 
 
         $articleSearchQueryOptions = new ArticleSearchQueryOptions($page, $perPage, $query,
